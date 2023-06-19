95b905f2162b1f4195829aa55275787ad4a3aa3f
annaReichelt <anna.m.reichelt@gmail.com>
2023-06-19 19:50:36 +0200

    Added test passengers

--- src/Policy/Happiness.java
+++ src/Policy/Happiness.java
@@ -1,13 +1,11 @@
 package src.Policy;
 
-//I wanted to do enums because it would be easier to change for the different policies but Im sure theres a better way
-
 public enum Happiness {
 
      SMALL_MALUS(10), 
      NORMAL_MALUS(25), 
      LARGE_MALUS(50),
-     CANCELED_MALUS(100);
+     CANCELED_MALUS(100); //negative happyness = canceled
 
      private final int value;
      Happiness(int value) { this.value = value; }
